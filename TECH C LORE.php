12469b8c4dc16baa42359e8b61ef6a496e1ff651
imdanield2nd <imdanield2nd@gmail.com>
2021-02-02 18:42:10 +0800

    Add my files

---
+++ TECH C LORE.php
@@ -0,0 +1,10 @@
+<?php
+ echo "Git 101!";
+echo "Tech C Lore";
+echo "Members:";
+echo "Arnaiz, John Paul B.";
+echo "Asas, Gennesaret D.";
+echo "Baking, Daniel Joshua C.";
+echo "Daracan, Daryll Jade B.";
+echo "Samia, Angel";
+?>
